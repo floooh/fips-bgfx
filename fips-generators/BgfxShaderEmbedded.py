cf65aa608ef13e89705e5b4e31460acd1c049777
Danny Angelo Carminati Grein <fungos@gmail.com>
2015-04-02 00:26:26 -0400

    Solves tools dependency for building shader compiler and building shaders in the correct order. Requires floooh/fips#81 to work.

--- fips-generators/BgfxShaderEmbedded.py
+++ fips-generators/BgfxShaderEmbedded.py
@@ -9,17 +9,32 @@ import platform
 import genutil
 import tempfile
 import subprocess
+import yaml
 from mod import log
+from mod import util
+
+# HACK: Find fips-deploy dir the hard way
+# TODO: Fips need pass to generators the fips-deploy dir ready to be used
+deploy_path = ""
+with open(".fips-settings.yml", 'r') as f:
+    items = yaml.load(f)
+    cfg = {}
+    cfg['name'] = items['config']
+    deploy_path = util.get_deploy_dir("../fips", "fips-bgfx", cfg)
 
 #-------------------------------------------------------------------------------
 def get_shaderc_path() :
     """find shaderc compiler, fail if not exists"""
-    proj_path = os.path.dirname(os.path.abspath(__file__))
-    os_name = platform.system().lower()
-    shaderc_path = '{}/../bgfx/tools/bin/{}/shaderc'.format(proj_path, os_name)
-    shaderc_path = os.path.normpath(shaderc_path)
+
+    shaderc_path = os.path.abspath('{}/shaderc'.format(deploy_path))
     if not os.path.isfile(shaderc_path) :
-        log.error("bgfx shaderc executable not found, please run 'make tools' in bgfx directory")
+        proj_path = os.path.dirname(os.path.abspath(__file__))
+        os_name = platform.system().lower()
+        shaderc_path = '{}/../bgfx/tools/bin/{}/shaderc'.format(proj_path, os_name)
+        shaderc_path = os.path.normpath(shaderc_path)
+        if not os.path.isfile(shaderc_path) :
+            log.error("bgfx shaderc executable not found, please run 'make tools' in bgfx directory")
+
     return shaderc_path
 
 #-------------------------------------------------------------------------------
@@ -53,16 +68,23 @@ def run_shaderc(input_file, out_tmp, platform, type, subtype, bin_name) :
         '-o', out_tmp,
         '--bin2c', bin_name
     ])
-    print(cmd)
     subprocess.call(cmd)
 
 #-------------------------------------------------------------------------------
-def generate(input_file, out_src, out_hdr, args) :
+def generate(input_file, out_src, out_hdr) :
     """
     :param input:       bgfx .sc file
     :param out_src:     must be None
     :param out_hdr:     path of output header file
     """
+
+    # deduce shader type
+    base_file = os.path.basename(input_file)
+    shader_type = "vertex"
+    if base_file.startswith("fs_"):
+        shader_type = "fragment"
+    if base_file.startswith("cs_"):
+        shader_type = "compute"
 
     # source to bgfx shader compiler
     shaderc_path = get_shaderc_path()
@@ -76,13 +98,31 @@ def generate(input_file, out_src, out_hdr, args) :
 
     # FIXME: the HLSL compiler is only supported on Windows platforms,
     # thus we would get incomplete .bin.h files on non-windows platforms...
-    # what to do about this:
+    contents = ""
 
-    run_shaderc(input_file, out_glsl, 'linux', args['type'], None, basename+'_glsl')
-    run_shaderc(input_file, out_dx9, 'windows', args['type'], 
-                'vs_3_0' if args['type'] == 'vertex' else 'ps_3_0', basename+'_dx9')
-    run_shaderc(input_file, out_dx11, 'windows', args['type'],
-                'vs_4_0' if args['type'] == 'vertex' else 'ps_4_0', basename+'_dx11')
+    os_name = platform.system().lower()
+    run_shaderc(input_file, out_glsl, 'linux', shader_type, None, basename+'_glsl')
+    with open(out_glsl, 'r') as f:
+        contents += f.read()
 
-    # TODO: merge output temp files into 
+    if os_name == 'windows':
+        run_shaderc(input_file, out_dx9, 'windows', shader_type, 
+                'vs_3_0' if shader_type == 'vertex' else 'ps_3_0', basename+'_dx9')
+        run_shaderc(input_file, out_dx11, 'windows', shader_type,
+                'vs_4_0' if shader_type == 'vertex' else 'ps_4_0', basename+'_dx11')
 
+        with open(out_dx9, 'r') as f:
+            contents += f.read()
+        
+        with open(out_dx11, 'r') as f:
+            contents += f.read()
+    else:
+        contents += "\n"
+        contents += "// built on {}, hlsl compiler not disponible\n".format(os_name)
+        contents += "static const uint8_t {}_dx9[1] = {{ 0 }};\n\n".format(basename)
+        contents += "// built on {}, hlsl compiler not disponible\n".format(os_name)
+        contents += "static const uint8_t {}_dx11[1] = {{ 0 }};\n\n".format(basename)
+
+    if len(contents):
+        with open(out_hdr, 'w') as f:
+            f.write(contents)
